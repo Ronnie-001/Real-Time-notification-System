f9676777dca67575ebf3be33c1767a1d68292004
Ronnie-001 <ronald.adesida@gmail.com>
2025-09-17 14:57:51 +0100

    Add: set up database connection & add schema for getting login details.

---
+++ services/scraping-service/app/database/dbconn.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import declarative_base
+
+# get the specific path to the .env file
+env_path = Path(__file__).parent / '.env'
+
+# load the .env file
+load_dotenv(dotenv_path=env_path, override=True)
+
+# get the database url from the .env file
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Check that the DB connection string has been set as an evironment variable
+if DATABASE_URL is None:
+    raise ValueError(
+        "DATABASE_URL is not set."
+        "Please check that the .env file exists at the correct path "
+        "and contains the DATABASE_URL variable."
+    )
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+Session = async_sessionmaker(engine, expire_on_commit=False)
+Base = declarative_base()
--- services/scraping-service/main.py
+++ services/scraping-service/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routes.scrapingRouter import scrapingRouter
+from app.database.dbconn import DATABASE_URL
 
 app = FastAPI()
 
@@ -7,6 +8,8 @@ app = FastAPI()
 @app.get("/scraping-service/v1/test")
 async def root() -> str:
     return "Working!"
+
+print(DATABASE_URL)
 
 # include the different routers.
 app.include_router(scrapingRouter)
